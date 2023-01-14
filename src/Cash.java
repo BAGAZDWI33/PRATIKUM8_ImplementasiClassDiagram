8f3725b3dceb3be8c75059220e14bb4916a7914a
BASMA33 <bagasdwiprasetyo96@gmail.com>
2023-01-14 15:26:38 +0700

    upload file

--- src/Cash.java
+++
@@ -1,7 +0,0 @@
-public class Cash {
-    public float cashTendered;
-
-    public Cash(float cashTendered){
-        this.cashTendered=cashTendered;
-    }
-}
--- src/Payment.java
+++
@@ -1,7 +0,0 @@
-public class Payment {
-    public float amount;
-    public Payment(float amount){
-        this.amount = amount;
-    }
-}
-
